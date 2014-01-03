a76c110114a312870be014080695f9ed7cf7bab5
tlei <terry.haotian.lei@gmail.com>
2014-01-02 22:15:01 -0500

    calendar now renders classes, added a few test data sets

--- js/entrypoint.js
+++ js/entrypoint.js
@@ -1,8 +1,52 @@
 $(function () {
-    new Scheduler.views.Calendar({
-        "el": "#scheduleContainer",
-        "startTime": 9 * 60,
-        "endTime": 22 * 60 + 30,
-        "model": new Scheduler.models.SectionCollection()
-    });
+	new Scheduler.views.Calendar({
+		"el": "#scheduleContainer",
+		"startTime": 9 * 60,
+		"endTime": 22 * 60 + 30,
+		"sectionList": new Scheduler.models.SectionCollection([
+			{
+				"subject": "STV",
+				"catalog_number": "100",
+				"units": 0.5,
+				"title": "Society, Technology and Values: Introduction",
+				"note": null,
+				"class_number": 6928,
+				"section": "LEC 001",
+				"campus": "UW U",
+				"associated_class": 1,
+				"related_component_1": null,
+				"related_component_2": null,
+				"enrollment_capacity": 80,
+				"enrollment_total": 80,
+				"waiting_capacity": 0,
+				"waiting_total": 0,
+				"topic": null,
+				"reserves": [],
+				"classes": [
+				{
+			        "dates": {
+			            "start_time": "18:00",
+			            "end_time": "21:30",
+			            "weekdays": "TTh",
+			            "start_date": null,
+			            "end_date": null,
+			            "is_tba": false,
+			            "is_cancelled": false,
+			            "is_closed": false
+			        },
+			        "location": {
+			            "building": "E5",
+			            "room": "6004"
+			        },
+			        "instructors": [
+			            "Campbell,Scott Martin"
+			        ]
+			    }],
+				"held_with": [],
+				"term": 1141,
+				"academic_level": "undergraduate",
+				"last_updated": "2013-12-31T18:02:45-05:00"
+			}
+		])
+	});
 });
